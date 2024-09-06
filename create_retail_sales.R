77cc8566e2be961e8401302ca4db45ac84eefc46
Ian Gow <iandgow@gmail.com>
2024-09-06 19:45:48 +1000

    Switched to `readabs`.

--- create_retail_sales.R
+++ create_retail_sales.R
@@ -32,5 +32,32 @@ retail_sales <-
   separate_wider_regex(industry,
                        c(industry_code = ".*", ":\\s+", industry = ".*")) |>
   select(-region_code, -industry_code) |>
+  distinct() |>
+  arrange(time_period, industry, region) |>
+  select(-obs_status) |>
+  write_csv("data/retail_sales.csv") |>
+  system_time()
+
+library(readabs)
+
+Sys.setenv(R_READABS_PATH = "~/Downloads/")
+
+retail_sales <-
+  read_abs("8501.0") |>
+  filter(series_type == "Original") |>
+  filter(data_type == "FLOW",
+         table_title == "TABLE 11. Retail Turnover, State by Industry Subgroup, Original") |>
+  separate_wider_regex(series,
+                       c("^.*;\\s+", region = ".*?", "\\s+;\\s+", industry = ".*?", "\\s+;$")) |>
+  rename(obs_value = value,
+         time_period = date) |>
+  mutate(obs_value = round(obs_value, 1)) |>
+  select(industry, region, time_period, obs_value) |>
+  mutate(region = as.character(region)) |>
+  mutate(
+    industry = if_else(industry == "Total (Industry)", "Total", industry),
+    region = if_else(region == "Total (State)", "Australia", region))|>
+  distinct() |>
+  arrange(time_period, industry, region) |>
   write_csv("data/retail_sales.csv") |>
   system_time()
