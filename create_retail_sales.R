99ee30391f68e316f8b97f8f68a1bdb71d50d1c9
Ian Gow <iandgow@gmail.com>
2024-09-07 07:03:58 +1000

    More edits to `retail_sales` code.

--- create_retail_sales.R
+++ create_retail_sales.R
@@ -1,46 +1,7 @@
-library(tidyverse)
-library(duckdb)
-
-system_time <- function (expr) {
-  print(system.time(expr))
-  expr
-}
-
-db <- dbConnect(duckdb())
-
-fix_names <- function(x) {
-  str_replace(x, ":.*$", "") |>
-    str_to_lower()
-}
-
-csv_file <- "~/Downloads/ABS_RT_1.0.0.csv"
-
-retail_sales <-
-  tbl_file(db, csv_file) |>
-  rename_with(fix_names) |>
-  filter(unit_measure == "AUD: Australian Dollars",
-         measure == "M1: Current Prices",
-         tsest == "10: Original",
-         freq == "M: Monthly",
-         unit_mult == "6: Millions") |>
-  select(-unit_measure, -measure, -tsest, -freq, -unit_mult,
-         -obs_comment, -dataflow) |>
-  mutate(time_period = as.Date(str_c(time_period, "-01"))) |>
-  collect() |>
-  separate_wider_regex(region,
-                       c(region_code = ".*", ":\\s+", region = ".*")) |>
-  separate_wider_regex(industry,
-                       c(industry_code = ".*", ":\\s+", industry = ".*")) |>
-  select(-region_code, -industry_code) |>
-  distinct() |>
-  arrange(time_period, industry, region) |>
-  select(-obs_status) |>
-  write_csv("data/retail_sales.csv") |>
-  system_time()
-
-library(readabs)
-
 Sys.setenv(R_READABS_PATH = "~/Downloads/")
+
+library(tidyverse)
+library(readabs)
 
 retail_sales <-
   read_abs("8501.0") |>
@@ -48,16 +9,115 @@ retail_sales <-
   filter(data_type == "FLOW",
          table_title == "TABLE 11. Retail Turnover, State by Industry Subgroup, Original") |>
   separate_wider_regex(series,
-                       c("^.*;\\s+", region = ".*?", "\\s+;\\s+", industry = ".*?", "\\s+;$")) |>
-  rename(obs_value = value,
-         time_period = date) |>
-  mutate(obs_value = round(obs_value, 1)) |>
-  select(industry, region, time_period, obs_value) |>
-  mutate(region = as.character(region)) |>
-  mutate(
-    industry = if_else(industry == "Total (Industry)", "Total", industry),
-    region = if_else(region == "Total (State)", "Australia", region))|>
+                       c("^.*;\\s+", state = ".*?", "\\s+;\\s+", industry = ".*?", "\\s+;$")) |>
+  select(industry, state, date, value) |>
   distinct() |>
-  arrange(time_period, industry, region) |>
-  write_csv("data/retail_sales.csv") |>
-  system_time()
+  arrange(date, industry, state) |>
+  write_csv("data/retail_sales.csv")
+
+library(readxl)
+
+url <- "https://api.data.abs.gov.au/dataflow/all?detail=allstubs"
+t <- tempfile()
+download.file(url, t)
+
+library(xml2)
+
+
+temp <-
+  read_xml(t) |>
+  xml_child(2) |>
+  xml_child(1) |>
+  xml_contents() |>
+  xml_attrs()
+
+extract_data <- function(x) tibble(data.frame(t(x)))
+
+df <- map(temp, extract_data) |> bind_rows()
+df
+url <- "https://api.data.abs.gov.au/datastructure/ABS/RT/M..M1..10.AUD.."
+url <- "https://api.data.abs.gov.au/codelist/ABS/RT/1.0.0"
+
+url <- "https://api.data.abs.gov.au/dataflow/ABS/RT"
+url <- "https://api-reg.data.abs.gov.au/DisseminateNSIService/rest/dataflow/ABS/RT/1.0.0"
+read_sdmx("/text/csv/ABS/{structureId}")
+
+sdmx <- readSDMX(providerId = "ABS", resource = "data", flowRef = "RT",
+                 key = list(NULL, NULL, NULL), start = 2010, end = 2011)
+df <- as.data.frame(sdmx)
+
+dsdUrl <- "https://api.data.abs.gov.au/datastructure/ABS/RT"
+dsdUrl <- "https://api.data.abs.gov.au/datastructure/ABS/RT?references=codelist"
+cls <- slot(dsd, "codelists")
+codelists <- sapply(slot(cls, "codelists"), function(x) slot(x, "id"))
+codelists
+
+codelist <- as.data.frame(slot(dsd, "codelists"), codelistId = "CL_STATE")
+codelist
+vdsdUrl <- "https://api.data.abs.gov.au/datastructure/ABS/RT?references=codelist"
+cls <- slot(dsd, "codelists")
+codelists <- sapply(slot(cls, "codelists"), function(x) slot(x, "id"))
+codelists
+
+
+https://api.data.abs.gov.au/datastructure/ABS/ALC
+dsd <- readSDMX(dsdUrl)
+
+
+concepts <- as.data.frame(slot(dsd, "concepts"))
+concepts
+
+
+codelists
+
+codelist
+attributes(dsd)
+
+concepts <- as.data.frame(slot(dsd, "concepts"))
+concepts
+
+sdmx.data <- readSDMX(providerId = "OECD", resource = "data", flowRef = "MIG",
+                      key = list("TOT", NULL, NULL), start = 2010, end = 2011)
+
+sdmx.data <- readSDMX(providerId = "ABS", resource = "data", flowRef = "RT",
+                      key = list("M1", NULL, 10, NULL, "M"))
+df <- as_tibble(sdmx.data)
+
+
+dsd <- readSDMX(providerId = "ABS", resource = "datastructure",
+                resourceId = "RT")
+
+#get codelists from DSD
+cls <- slot(dsd, "codelists")
+codelists <- sapply(slot(cls,"codelists"), slot, "id") #get list of codelists
+
+#get a codelist
+states <-
+  as.data.frame(cls, codelistId = "CL_STATE") |>
+  rename(state = label.en)
+industries <-
+  as.data.frame(cls, codelistId = "CL_RT_IND") |>
+  rename(industry = label.en.label)
+
+
+df |>
+  inner_join(states, join_by(REGION == id)) |>
+  inner_join(industries, join_by(INDUSTRY == id)) |>
+  select(obsTime, industry, state, obsValue)
+
+
+codelist
+#get concepts from DSD
+concepts <- as.data.frame(slot(dsd, "concepts"))
+
+
+res <- readSDMX("https://api.data.abs.gov.au/data/ABS,RT,1.0.0/M1.20.10.AUS.M?dimensionAtObservation=AllDimensions")
+res <- readSDMX("https://api.data.abs.gov.au/data/ABS,RT,1.0.0/M1..10..M&format=csv")
+res <- readSDMX("https://api.data.abs.gov.au/data/ABS,RT,1.0.0/M1..10..M?dimensionAtObservation=AllDimensions")
+
+df
+#DSD
+sdmx.dsd <- readSDMX(providerId = "OECD", resource = "datastructure", resourceId = "MIG")
+
+#associate data and dsd
+sdmx.data <- setDSD(sdmx.data, sdmx.dsd)
--- plots_retail_sales.R
+++ plots_retail_sales.R
@@ -1,36 +1,30 @@
 library(tidyverse)
 
-system_time <- function (expr) {
-  print(system.time(expr))
-  expr
-}
-
-retail_sales <-
-  read_csv("data/retail_sales.csv", show_col_types = FALSE) |>
-  system_time()
+retail_sales <- read_csv("data/retail_sales.csv",
+                         show_col_types = FALSE)
 
 retail_sales |>
-  filter(region == "Australia") |>
-  filter(between(time_period, as.Date("2010-01-01"), as.Date("2024-08-31"))) |>
-  ggplot(aes(x = time_period, y = obs_value)) +
+  filter(state == "Total (State)") |>
+  filter(between(date, as.Date("2010-01-01"), as.Date("2024-08-31"))) |>
+  ggplot(aes(x = date, y = value)) +
   geom_line() +
   facet_wrap(industry ~ ., ncol = 3, scales = "free_y")
 
 liquor <-
   retail_sales |>
-  filter(region %in% c("Victoria", "Western Australia"),
+  filter(state %in% c("Victoria", "Western Australia"),
          industry == "Liquor retailing") |>
-  filter(between(time_period, as.Date("2018-01-01"), as.Date("2024-08-31"))) |>
-  mutate(year = year(time_period),
-         month = month(time_period, label = TRUE))
+  filter(between(date, as.Date("2018-01-01"), as.Date("2024-08-31"))) |>
+  mutate(year = year(date),
+         month = month(date, label = TRUE))
 
 liquor |>
   filter(year == 2018) |>
-  group_by(region) |>
-  summarize(scale = mean(obs_value)) |>
-  inner_join(liquor, by = "region") |>
-  mutate(obs_value = obs_value / scale) |>
-  ggplot(aes(x = month, y = obs_value, fill = region)) +
+  group_by(state) |>
+  summarize(scale = mean(value)) |>
+  inner_join(liquor, by = "state") |>
+  mutate(value = value / scale) |>
+  ggplot(aes(x = month, y = value, fill = state)) +
   geom_col(position = "dodge") +
   facet_wrap(year ~ ., ncol = 2) +
   theme(legend.position = "bottom") +
